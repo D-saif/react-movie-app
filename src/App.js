de605b4403fadab30d3aef88287c1848be0c2feb
Saif DHIFI <saifeddine.dhifi@infraplus.net>
2022-07-01 15:36:59 +0100

    it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchIcon from "./SearchIcon.svg";
+import { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+//fa3fef62
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'http://www.omdbapi.com/?i=tt3896198&apikey=fa3fef62';
+
+const movie1 = {
+     Title: "The Amazing Spiderman T4 Premiere Special",
+     Year: "2012",
+     imdbID: "tt2233044",
+     Type: "movie",
+     Poster: "N/A"
+    };
+
+const App = () =>{
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title)=>{
+        const response = await fetch(`${API_KEY}&s=${title}`)
+        const data = await response.json();
+        console.log(data);
+        data.Response === "True" ?
+        (setMovies(data.Search)) :
+        (setMovies([]))
+        
+        
+    }
+
+    useEffect(()=>{
+        searchMovies("spiderman")
+    },[])
+
+
+    return(
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input placeholder="search for a movie" 
+                    value={searchTerm}
+                    onChange={(e)=>{setSearchTerm(e.target.value);searchMovies(searchTerm)}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={()=>{searchMovies(searchTerm)}}
+                />
+            </div>
+            {
+                movies.length > 0 ?
+
+                (<div className="container">
+                    {movies.map((movie)=> (
+                        <MovieCard movie = {movie}/>
+                    ))}
+                </div>) :
+
+                (<div className="empty">
+                    <h3>NO MOVIES FOUND</h3>
+                </div>)
+
+            }
+        </div>
+    );
 }
-
+ 
 export default App;
